434c0780df00536ae92d1e9fe751ad56fdfdad7b
Adam Margherio <amargherio@microsoft.com>
2025-09-15 17:18:07 -0500

    building out handling for github pages publishing

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -2,12 +2,17 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import svelteConfig from './svelte.config.js';
 
-export default defineConfig({
+const REPO = "gateway-api-visualizer";
+
+export default defineConfig(({ mode }) => ({
+  base: mode === 'development' ? '/' : `/${REPO}/`,
   plugins: [svelte({ ...svelteConfig })],
   server: {
     port: 5173
   },
   build: {
+    outDir: 'dist',
+    emptyOutDir: true,
     rollupOptions: {
       output: {
         manualChunks: {
@@ -18,4 +23,4 @@ export default defineConfig({
       }
     }
   }
-});
+}));
---
+++ scripts/post-build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cp dist/index.html dist/404.html
